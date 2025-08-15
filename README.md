1537d1d8233cc5eed414103bb51eed2e870b64d0
Dogend <yanshengwys@qq.com>
2025-08-16 00:25:04 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# helloworld
+# JOJO替身面板
 
-AstrBot 插件模板
+调用TripleYing的API生成JOJO替身面板图片
 
-A template plugin for AstrBot plugin feature
+[完整版](http://www.tripleying.com/jojo)
 
-# 支持
+# 插件配置
 
-[帮助文档](https://astrbot.app)
+API服务器地址：获取图片的url
+
+群聊白名单：可以触发指令的群聊
+
+# 插件指令
+
+随机替身：随机生成一个替身面板图片
+
+今日替身：用户当天的随机替身（使用ID和日期作为种子）
--- main.py
+++ main.py
@@ -1,24 +1,80 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core.platform.message_type import MessageType
+import astrbot.api.message_components as Comp
+from astrbot.api import AstrBotConfig
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+import random
+import datetime
+import pytz
+from urllib.parse import urlencode
+
+@register("astrbot_plugin_jojo_stand_panel", "Dogend", "调用TripleYing的API生成JOJO替身面板图片", "1.0.0")
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+
+    api_server = "https://api.tripleying.com/api/chart"
+    time_zone = pytz.timezone('Asia/Shanghai')
+    group_white_list = []
+
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.api_server = config.get("api_server")
+        self.group_white_list = config.get("white_list")
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+
+    def get_image_url(self, name: str = None, ability: str = None):
+        params = {}
+        if name is not None:
+            params['name'] = name
+        if ability is not None:
+            params['ability'] = ability
+        if not params:
+            return self.api_server
+        query_string = urlencode(params)
+        return f"{self.api_server}?{query_string}"
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    @filter.command("随机替身")
+    async def random_stand(self, event: AstrMessageEvent):
+        if event.get_message_type() == MessageType.GROUP_MESSAGE:
+            if event.get_group_id() not in self.group_white_list:
+                logger.info("群聊不在白名单中:"+event.get_group_id())
+                return
         user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        image_url = self.get_image_url(name = user_name)
+        chain = [
+            Comp.Plain("你抽到的替身面板是："),
+            Comp.Image.fromURL(image_url),
+        ]
+        yield event.chain_result(chain)
+
+    @filter.command("今日替身")
+    async def today_stand(self, event: AstrMessageEvent):
+        if event.get_message_type() == MessageType.GROUP_MESSAGE:
+            if event.get_group_id() not in self.group_white_list:
+                logger.info("群聊不在白名单中:"+event.get_group_id())
+                return
+        user_id = event.get_sender_id()
+        user_name = event.get_sender_name()
+        ability_arr = []
+        current_date = datetime.datetime.now(self.time_zone).strftime("%Y%m%d")
+        seed = f"{user_id}{current_date}"
+        person_random = random.Random(seed)
+
+        for i in range(6):
+            ability_arr.append(str(person_random.randint(1, 5)))
+        ability_str = ",".join(ability_arr)
+        image_url = self.get_image_url(
+            name = user_name,
+            ability = ability_str
+        )
+        chain = [
+            Comp.Plain("你抽到的替身面板是："),
+            Comp.Image.fromURL(image_url),
+        ]
+        yield event.chain_result(chain)
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
