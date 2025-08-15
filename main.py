64b87ef3766138d41d7e9c81d0cdcd647638d308
Dogend <yanshengwys@qq.com>
2025-08-16 01:37:02 +0800

    ruff format

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 from astrbot.core.platform.message_type import MessageType
-import astrbot.api.message_components as Comp
 from astrbot.api import AstrBotConfig
+import astrbot.api.message_components as Comp
 
 import random
 import datetime
 import pytz
 from urllib.parse import urlencode
 
-@register("astrbot_plugin_jojo_stand_panel", "Dogend", "调用TripleYing的API生成JOJO替身面板图片", "1.0.0")
-class MyPlugin(Star):
 
+@register(
+    "astrbot_plugin_jojo_stand_panel",
+    "Dogend",
+    "调用TripleYing的API生成JOJO替身面板图片",
+    "1.0.0",
+)
+class MyPlugin(Star):
     api_server = "https://api.tripleying.com/api/chart"
-    time_zone = pytz.timezone('Asia/Shanghai')
+    time_zone = pytz.timezone("Asia/Shanghai")
     group_white_list = []
 
     def __init__(self, context: Context, config: AstrBotConfig):
@@ -23,27 +28,27 @@ class MyPlugin(Star):
         self.group_white_list = config.get("white_list")
 
     async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        """插件初始化方法"""
 
     def get_image_url(self, name: str = None, ability: str = None):
         params = {}
         if name is not None:
-            params['name'] = name
+            params["name"] = name
         if ability is not None:
-            params['ability'] = ability
+            params["ability"] = ability
         if not params:
             return self.api_server
         query_string = urlencode(params)
         return f"{self.api_server}?{query_string}"
-    
+
     @filter.command("随机替身")
     async def random_stand(self, event: AstrMessageEvent):
         if event.get_message_type() == MessageType.GROUP_MESSAGE:
             if event.get_group_id() not in self.group_white_list:
-                logger.info("群聊不在白名单中:"+event.get_group_id())
+                logger.info("群聊不在白名单中:" + event.get_group_id())
                 return
         user_name = event.get_sender_name()
-        image_url = self.get_image_url(name = user_name)
+        image_url = self.get_image_url(name=user_name)
         chain = [
             Comp.Plain("你抽到的替身面板是："),
             Comp.Image.fromURL(image_url),
@@ -54,7 +59,7 @@ class MyPlugin(Star):
     async def today_stand(self, event: AstrMessageEvent):
         if event.get_message_type() == MessageType.GROUP_MESSAGE:
             if event.get_group_id() not in self.group_white_list:
-                logger.info("群聊不在白名单中:"+event.get_group_id())
+                logger.info("群聊不在白名单中:" + event.get_group_id())
                 return
         user_id = event.get_sender_id()
         user_name = event.get_sender_name()
@@ -66,10 +71,7 @@ class MyPlugin(Star):
         for i in range(6):
             ability_arr.append(str(person_random.randint(1, 5)))
         ability_str = ",".join(ability_arr)
-        image_url = self.get_image_url(
-            name = user_name,
-            ability = ability_str
-        )
+        image_url = self.get_image_url(name=user_name, ability=ability_str)
         chain = [
             Comp.Plain("你抽到的替身面板是："),
             Comp.Image.fromURL(image_url),
@@ -77,4 +79,4 @@ class MyPlugin(Star):
         yield event.chain_result(chain)
 
     async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        """插件销毁方法"""
